9863e14d9600dff39f6d46293411ab444d1ea0a7
vizantija037 <80783335+vizantija037@users.noreply.github.com>
2022-03-05 13:56:45 +0100

    Add files via upload

--- Script.js
+++ Script.js
@@ -33,3 +33,89 @@ function validateMessage(e){
         alert('\nMessage NOT sent!!!\n\nFill in all the fields in the contact form!');   
         
 }}
+
+
+window.addEventListener('scroll', ()=>{
+    let content = document.querySelector('.section-5');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight/1.2;
+    if(contentPosition < screenPosition){
+        
+        content.classList.add('active');
+    } else {
+        content.classList.remove('active')
+    }
+})
+
+window.addEventListener('scroll', ()=>{
+    let content = document.querySelector('.section-6');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight/1.2;
+    if(contentPosition < screenPosition){
+        console.log('ovde');
+        content.classList.add('active-6');
+    } else {
+        content.classList.remove('active-6')
+    }
+})
+window.addEventListener('scroll', ()=>{
+    let content = document.querySelector('.section-7');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight/1.2;
+    if(contentPosition < screenPosition){
+        console.log('ovde');
+        content.classList.add('active-7');
+    } else {
+        content.classList.remove('active-7')
+    }
+})
+window.addEventListener('scroll', ()=>{
+    let content = document.querySelector('.section-8');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight/1.2;
+    if(contentPosition < screenPosition){
+        console.log('ovde');
+        content.classList.add('active-8');
+    } else {
+        content.classList.remove('active-8')
+    }
+})
+window.addEventListener('scroll', ()=>{
+    let content = document.querySelector('.section-9');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight/1.2;
+    if(contentPosition < screenPosition){
+        console.log('ovde');
+        content.classList.add('active-9');
+    } else {
+        content.classList.remove('active-9')
+    }
+})
+
+window.addEventListener('scroll', ()=>{
+    let content = document.querySelector('.section-9');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight/1.2;
+    if(contentPosition < screenPosition){
+        console.log('ovde');
+        content.classList.add('active-9');
+    } else {
+        content.classList.remove('active-9')
+    }
+})
+
+window.addEventListener('scroll', ()=>{
+    let content = document.querySelector('.section-10');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight/1.6;
+    if(contentPosition < screenPosition){
+        console.log('ovde');
+        content.classList.add('active-10');
+    } else {
+        content.classList.remove('active-10')
+    }
+})
+
+
+
+
